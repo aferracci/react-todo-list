2ffa692b7771f9607af939c13aee6dde40f68978
FERRACCI Andrea 505335 <andrea.ferracci@bnlmail.com>
2019-11-07 00:26:08 +0100

    showing vscode github tool

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
