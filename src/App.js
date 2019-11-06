542512f82bdd5088cec826bf71401eef9736b514
FERRACCI Andrea 505335 <andrea.ferracci@bnlmail.com>
2019-11-07 00:12:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return ( 
+  <div>
+    <div className = "container" >
+      <div className = "row" >
+        <TodoInput/>
+        <TodoList/>
+      </div> 
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import uuid from 'uuid';
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                hello from Todo Item
+            </div>
+        )
+    }
+}
