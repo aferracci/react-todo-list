f3ab75f181bb08f53448a23394776a27912e9649
FERRACCI Andrea 505335 <andrea.ferracci@bnlmail.com>
2019-11-09 23:44:39 +0100

    All basic functionality completed

--- src/App.js
+++ src/App.js
@@ -36,9 +36,29 @@ class App extends Component {
     }, () => console.log(this.state));
   };
 
-  clearList = () => {console.log('clear list')}
-  handleDelete = (id) => {console.log('handle delete')}
-  handleEdit = (id) => {console.log('handle edit')}
+  clearList = () => {
+    this.setState({
+      items: []
+    });
+  }
+
+  handleDelete = (id) => {
+    const filteredItems = this.state.items.filter(item => item.id != id);
+    this.setState({
+      items: filteredItems
+    });
+  }
+
+  handleEdit = (id) => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    const selectedItem = this.state.items.find(item => item.id === id);
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true
+    });
+  }
   
   render() {
     return (
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-import Item from "./TodoItem";
+import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 export default class TodoList extends Component {
@@ -7,7 +6,7 @@ export default class TodoList extends Component {
         const {items, clearList, handleDelete, handleEdit} = this.props;
         return (
             <ul className="list-group my-5">
-                <h3 className="text-capitalize text-center"></h3>
+                <h3 className="text-capitalize text-center"> todo list</h3>
                 {items.map(item => {
                     return (<TodoItem 
                         key={item.id}
